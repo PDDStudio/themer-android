dc08ffee65f09bcab440f9106664b23307ac8b70
Patrick J <patrick.pddstudio@googlemail.com>
2017-09-25 13:42:49 +0200

    Updated tests to match with new template

--- index.spec.js
+++ index.spec.js
@@ -1,7 +1,7 @@
 const {render} = require('./index');
 const {colors} = require('themer-colors-default');
 
-describe('themer Alfred.app theme template', () => {
+describe('themer Android theme template', () => {
   it('should render properly', done => {
     Promise.all(render(colors)).then(files => {
       expect(files.length).toBe(2);
