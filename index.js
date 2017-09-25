00b8ce47e51c9ef0a17e01d53707d733b1930052
Patrick J <patrick.pddstudio@googlemail.com>
2017-09-25 13:42:33 +0200

    Updated template to generate valid android resource colors

--- index.js
+++ index.js
@@ -1,99 +1,54 @@
+var xml = require('xml');
+
 const render = (colors, options) => {
-  const colorSets = [
-    {name: 'dark', colors: colors.dark},
-    {name: 'light', colors: colors.light},
-  ].filter(colorSet => !!colorSet.colors);
-  return colorSets.map(colorSet => {
-    const {
-      shade0,
-      shade1,
-      shade2,
-      shade3,
-      shade4,
-      shade5,
-      shade6,
-      shade7,
-      accent0,
-      accent1,
-      accent2,
-      accent3,
-      accent4,
-      accent5,
-      accent6,
-      accent7,
-    } = colorSet.colors;
-    return Promise.resolve({
-      name: `themer-alfred-${colorSet.name}.alfredappearance`,
-      contents: Buffer.from(
-        JSON.stringify(
-          {
-            alfredtheme: {
-              result: {
-                textSpacing: 6,
-                subtext: {
-                  size: 12,
-                  colorSelected: `${shade1}FF`,
-                  font: 'System Light',
-                  color: `${shade4}FF`,
-                },
-                shortcut: {
-                  size: 16,
-                  colorSelected: `${shade1}FF`,
-                  font: 'System',
-                  color: `${shade6}FF`,
-                },
-                backgroundSelected: `${accent4}FF`,
-                text: {
-                  size: 18,
-                  colorSelected: `${shade0}FF`,
-                  font: 'System Light',
-                  color: `${shade7}FF`,
-                },
-                iconPaddingHorizontal: 6,
-                paddingVertical: 6,
-                iconSize: 36,
-              },
-              search: {
-                paddingVertical: 8,
-                background: `${shade2}7F`,
-                spacing: 10,
-                text: {
-                  size: 22,
-                  colorSelected: `${accent5}FF`,
-                  font: 'System',
-                  color: `${shade7}FF`,
-                },
-                backgroundSelected: `${accent7}FF`,
-              },
-              window: {
-                color: `${shade0}CC`,
-                paddingHorizontal: 10,
-                width: 560,
-                borderPadding: 0,
-                borderColor: `${shade0}00`,
-                blur: 15,
-                roundness: 2,
-                paddingVertical: 10,
-              },
-              credit: 'Themer',
-              separator: {
-                color: `${shade0}00`,
-                thickness: 0,
-              },
-              scrollbar: {
-                color: `${accent2}FF`,
-                thickness: 2,
-              },
-              name: `Themer ${colorSet.name === 'dark' ? 'Dark' : 'Light'}`,
-            },
-          },
-          null,
-          2
-        ),
-        'utf8'
-      ),
+    const colorSets = [
+      { name: 'dark', colors: colors.dark },
+      { name: 'light', colors: colors.light },  
+    ].filter(colorSet => !!colorSet.colors);
+
+    return colorSets.map(colorSet => {
+        const {
+            shade0,
+            shade1,
+            shade2,
+            shade3,
+            shade4,
+            shade5,
+            shade6,
+            shade7,
+            accent0,
+            accent1,
+            accent2,
+            accent3,
+            accent4,
+            accent5,
+            accent6,
+            accent7,
+          } = colorSet.colors;
+          var data = [ { resources: [
+              { color: [ { _attr: { 'android:name': `color_shade0_${colorSet.name}`} }, shade0 ] },
+              { color: [ { _attr: { 'android:name': `color_shade1_${colorSet.name}`} }, shade1 ] },
+              { color: [ { _attr: { 'android:name': `color_shade2_${colorSet.name}`} }, shade2 ] },
+              { color: [ { _attr: { 'android:name': `color_shade3_${colorSet.name}`} }, shade3 ] },
+              { color: [ { _attr: { 'android:name': `color_shade4_${colorSet.name}`} }, shade4 ] },
+              { color: [ { _attr: { 'android:name': `color_shade5_${colorSet.name}`} }, shade5 ] },
+              { color: [ { _attr: { 'android:name': `color_shade6_${colorSet.name}`} }, shade6 ] },
+              { color: [ { _attr: { 'android:name': `color_shade7_${colorSet.name}`} }, shade7 ] },
+
+              { color: [ { _attr: { 'android:name': `color_accent0_${colorSet.name}`} }, accent0 ] },
+              { color: [ { _attr: { 'android:name': `color_accent1_${colorSet.name}`} }, accent1 ] },
+              { color: [ { _attr: { 'android:name': `color_accent2_${colorSet.name}`} }, accent2 ] },
+              { color: [ { _attr: { 'android:name': `color_accent3_${colorSet.name}`} }, accent3 ] },
+              { color: [ { _attr: { 'android:name': `color_accent4_${colorSet.name}`} }, accent4 ] },
+              { color: [ { _attr: { 'android:name': `color_accent5_${colorSet.name}`} }, accent5 ] },
+              { color: [ { _attr: { 'android:name': `color_accent6_${colorSet.name}`} }, accent6 ] },
+              { color: [ { _attr: { 'android:name': `color_accent7_${colorSet.name}`} }, accent7 ] },
+            ]}];
+          return Promise.resolve({
+              name: `themer-android-${colorSet.name}.xml`,
+              contents: Buffer.from(xml(data, true)),
+          });
     });
-  });
 };
 
 module.exports = {render};
